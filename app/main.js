1c96e779d42181aac0f40af1bce4f22be4a2de14
LydiaVS <lidia.vazquez98@gmail.com>
2022-04-27 11:27:03 +0200

    js_1

--- app/main.js
+++ app/main.js
@@ -1,6 +1,36 @@
 window.onload = () => {
+   /* const img = document.querySelectorAll('.dog-img');
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(data =>{
+        img.setAttribute('src', data.message);
+    })*/
+   
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(data => {
+       let lista = document.querySelector(".lista");
+      //console.log(data)
+      for (const raza in data.message) {
 
+       //fetchImgDog(raza)
+       
+        //const imgperro = await fetch(`https://dog.ceo/api/breed/${raza}/images/random`);
+         //console.log(`${imgperro}`);
+         fetch(`https://dog.ceo/api/breed/${raza}/images/random`)
+            .then(res => res.json())
+            .then(data =>{
+                //img.setAttribute('src', data.message);
+                let item=`<li>
+                <span class="name">${raza}</span>
+                <img src="${ data.message}" alt="blabla"/>
+        </li>`;
+        lista.innerHTML += item;
+            })
+        
+       
+      }
 
-    
+    })
 
 }
