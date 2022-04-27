09637a3390df2d30a44c6092a8ec2f28e07a5148
LydiaVS <lidia.vazquez98@gmail.com>
2022-04-27 11:41:39 +0200

    detalle

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-   /* const img = document.querySelectorAll('.dog-img');
+   const img = document.querySelectorAll('.dog-img');
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(res => res.json())
     .then(data =>{
         img.setAttribute('src', data.message);
-    })*/
+    })
    
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(res => res.json())
@@ -13,17 +13,13 @@ window.onload = () => {
       //console.log(data)
       for (const raza in data.message) {
 
-       //fetchImgDog(raza)
-       
-        //const imgperro = await fetch(`https://dog.ceo/api/breed/${raza}/images/random`);
-         //console.log(`${imgperro}`);
          fetch(`https://dog.ceo/api/breed/${raza}/images/random`)
             .then(res => res.json())
             .then(data =>{
                 //img.setAttribute('src', data.message);
-                let item=`<li>
-                <span class="name">${raza}</span>
-                <img src="${ data.message}" alt="blabla"/>
+                let item=`<li onclick="window.location.href='detalle.html'" class="grid">
+                
+                <img src="${ data.message}" alt="${raza}"/>
         </li>`;
         lista.innerHTML += item;
             })
